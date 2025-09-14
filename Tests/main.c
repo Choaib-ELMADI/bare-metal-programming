cd112386d2ec8358838363c2531f8a59c20c393a
Choaib ELMADI <choaibamd@gmail.com>
2025-09-14 16:23:05 +0100

    Commands to stop at different stages of compilation

---
+++ Tests/main.c
@@ -0,0 +1,10 @@
+/* This is a first comment */
+// This is a second comment
+
+#define x (5)
+
+int main() {
+    int y = x;
+    return 0;
+}
+
---
+++ Tests/main.s
@@ -0,0 +1,37 @@
+	.cpu arm7tdmi
+	.arch armv4t
+	.fpu softvfp
+	.eabi_attribute 20, 1
+	.eabi_attribute 21, 1
+	.eabi_attribute 23, 3
+	.eabi_attribute 24, 1
+	.eabi_attribute 25, 1
+	.eabi_attribute 26, 1
+	.eabi_attribute 30, 6
+	.eabi_attribute 34, 0
+	.eabi_attribute 18, 4
+	.file	"main.c"
+	.text
+	.align	2
+	.global	main
+	.syntax unified
+	.arm
+	.type	main, %function
+main:
+	@ Function supports interworking.
+	@ args = 0, pretend = 0, frame = 8
+	@ frame_needed = 1, uses_anonymous_args = 0
+	@ link register save eliminated.
+	str	fp, [sp, #-4]!
+	add	fp, sp, #0
+	sub	sp, sp, #12
+	mov	r3, #5
+	str	r3, [fp, #-8]
+	mov	r3, #0
+	mov	r0, r3
+	add	sp, fp, #0
+	@ sp needed
+	ldr	fp, [sp], #4
+	bx	lr
+	.size	main, .-main
+	.ident	"GCC: (Arm GNU Toolchain 13.3.Rel1 (Build arm-13.24)) 13.3.1 20240614"
