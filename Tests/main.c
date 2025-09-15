79a6ec23cc109801b05e87cab2e91a33666701ec
Choaib ELMADI <choaibamd@gmail.com>
2025-09-15 23:22:12 +0100

    Analyze memory map files

--- Tests/main.c
+++ Tests/main.c
@@ -5,6 +5,14 @@
 
 void HardFault_Handler(void) {}
 
+const int var = 100;
+const char b = 'b';
+int init;
+int zero = 0;
+int one = 1;
+
+int array[x];
+
 int main(void) {
     int y = x;
     y++;
